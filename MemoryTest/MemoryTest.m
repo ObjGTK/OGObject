c6bda4449f5d28310c70f8b0c61561ee89603154
Lazarus <git@devbeejohn.de>
2024-02-01 08:40:07 +0100

    Improve tests (but linking OGObject only is broken currently).

--- MemoryTest/MemoryTest.m
+++ MemoryTest/MemoryTest.m
@@ -1,5 +1,5 @@
 #import <ObjFW/ObjFW.h>
-#import <OGEBookContacts/OGEBookContacts-Umbrella.h>
+#import <OGObject/OGObject.h>
 
 @interface MemoryTest: OFObject <OFApplicationDelegate>
 @end
@@ -10,84 +10,86 @@ OF_APPLICATION_DELEGATE(MemoryTest)
 - (void)applicationDidFinishLaunching: (OFNotification *)notification
 {
 	OFLog(@"Test series 1: Freeing memory via the mapper object.");
-	EContact *gecontact = E_CONTACT(e_contact_new());
-	g_object_add_weak_pointer (G_OBJECT (gecontact), (gpointer *) &gecontact);
+	GObject *gobject = g_object_new(G_TYPE_OBJECT, NULL);
+	g_object_add_weak_pointer (G_OBJECT (gobject), (gpointer *) &gobject);
 
-	OFLog(@"Starting ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	OFLog(@"Starting ref count of GObject is: %u", gobject->ref_count);
 	
 	OFLog(@"Creating a wrapper for GObject…");
-	OGEContact *myWrapper = [OGEContact wrapperFor:gecontact];
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	OGObject *myWrapper = [[OGObject alloc] init];
+	[myWrapper setGObject:gobject];
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Unrefing GObject…", nil);
-	g_object_unref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_unref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Retaining wrapper…", nil);
 	[myWrapper retain];
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Releasing wrapper…", nil);
 	[myWrapper release];
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Releasing wrapper…", nil);
 	[myWrapper release];
-	OFLog(@"GObject is NULL: %u", (gecontact == NULL));
+	OFLog(@"GObject is NULL: %u", (gobject == NULL));
 
 	OFLog(@"Test series 2: Freeing memory via the gobject.");
-	gecontact = E_CONTACT(e_contact_new());
-	g_object_add_weak_pointer (G_OBJECT (gecontact), (gpointer *) &gecontact);
+	gobject = g_object_new(G_TYPE_OBJECT, NULL);
+	g_object_add_weak_pointer (G_OBJECT (gobject), (gpointer *) &gobject);
 
-	OFLog(@"Starting ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	OFLog(@"Starting ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	
 	OFLog(@"Creating a wrapper for GObject…");
-	myWrapper = [OGEContact wrapperFor:gecontact];
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	myWrapper = [[OGObject alloc] init];
+	[myWrapper setGObject:gobject];
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Unrefing GObject…", nil);
-	g_object_unref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_unref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Refing GObject…", nil);
-	g_object_ref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_ref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Refing GObject…", nil);
-	g_object_ref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_ref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Refing GObject…", nil);
-	g_object_ref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_ref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Unrefing GObject…", nil);
-	g_object_unref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_unref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Unrefing GObject…", nil);
-	g_object_unref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_unref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Unrefing GObject…", nil);
-	g_object_unref(gecontact);
-	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gecontact))->ref_count));
+	g_object_unref(gobject);
+	OFLog(@"Ref count of GObject is: %u", (((GObject*)(gobject))->ref_count));
 	OFLog(@"Retain count of the wrapper is: %u", myWrapper.retainCount);
 
 	OFLog(@"Unrefing GObject…", nil);
-	g_object_unref(gecontact);
-	OFLog(@"GObject is NULL: %u", (gecontact == NULL));
+	g_object_unref(gobject);
+	OFLog(@"GObject is NULL: %u", (gobject == NULL));
 
 	[OFApplication terminate];
 }
