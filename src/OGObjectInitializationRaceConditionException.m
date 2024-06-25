cbb532249ff2e9ae18d19878fbd6127e05b9f1de
Letterus <git@devbeejohn.de>
2024-06-25 21:29:42 +0200

    Try to get closer to thread safety.

---
+++ src/OGObjectInitializationRaceConditionException.m
@@ -0,0 +1,25 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Johannes Brakensiek <objfw@devbeejohn.de>
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#import "OGObjectInitializationRaceConditionException.h"
+
+@implementation OGObjectInitializationRaceConditionException
+
+- (OFString *)description
+{
+	if (_inClass != nil)
+		return [OFString stringWithFormat:@"While coupling a GObject instance (or a child "
+		                                  @"instance) to an instance of class %@ "
+		                                  @"mismatching values were detected. It may be "
+		                                  @"assumed that a race condition occured.",
+		                 _inClass];
+	else
+		return @"While coupling a GObject instance (or a child "
+		       @"instance) to a (child)instance of class OGObject "
+		       @"mismatching values were detected. It may be "
+		       @"assumed that a race condition occured.";
+}
+
+@end
