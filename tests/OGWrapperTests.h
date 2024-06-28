5474b0bf5cf35f44512baddf2c2a73fad647121f
Letterus <git@devbeejohn.de>
2024-06-28 22:09:08 +0200

    Complete memory tests (failing right now).

---
+++ tests/OGWrapperTests.h
@@ -0,0 +1,14 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Johannes Brakensiek <objfw@devbeejohn.de>
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+#import "OGObjectTests.h"
+#import <ObjFW/ObjFW.h>
+
+OF_ASSUME_NONNULL_BEGIN
+
+@interface OGWrapperTests : OGObjectTests
+@end
+
+OF_ASSUME_NONNULL_END
